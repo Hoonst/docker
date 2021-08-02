7adcf88cad41c9169ca96ba283ae3bf6ecf46ce4
hoonst <yoonhs1993@nate.com>
2021-08-02 22:51:28 +0900

    generate_container.sh edit

--- generate_container.sh
+++ generate_container.sh
@@ -7,12 +7,22 @@ for ((argpos=1; argpos<$#; argpos++)); do
       argpos_plus1=$((argpos+1))
       image_name=${!argpos_plus1}
    fi
+   if [ "${!argpos}" == "--port1" ]; then
+      argpos_plus1=$((argpos+1))
+      port1=${!argpos_plus1}
+   fi
+   if [ "${!argpos}" == "--port2" ]; then
+      argpos_plus1=$((argpos+1))
+      port2=${!argpos_plus1}
+   fi
 done
 
 echo "Container_name: " $container_name
 echo "Image_name: " $image_name
+echo "Port1: " $port1
+echo "Port2: " $port2
 
 docker run --gpus all -td --ipc=host --name $container_name\
 	-v ~/gitRepo:/repo\
 	-v /etc/passwd:/etc/passwd\
-	-p 8890:8890 -p 6008:6008 $image_name
+	-p $port1:$port1 -p $port2:$port2 $image_name
